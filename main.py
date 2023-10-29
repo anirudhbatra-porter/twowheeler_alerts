52a5f9454eabb75589aca69596d3c819dc5fc62a
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-10-29 19:37:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import io
+from google.colab import files
+import math
+from dateutil import relativedelta
+import warnings
+warnings.filterwarnings("ignore")
+import snowflake.connector
