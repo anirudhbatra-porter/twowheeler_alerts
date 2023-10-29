212ba59e2613f923f8bc1ac06c547e15b6992875
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-10-29 19:28:37 +0530

    Create util.py

---
+++ util.py
@@ -0,0 +1,7 @@
+import subprocess
+
+packages_to_install = ['snowflake-connector-python==2.8.3', 'pandas', 'numpy', 'math', 'dateutil', 'warnings']
+
+def install_dependencies():
+  for package in packages_to_install:
+    subprocess.run(['pip', 'install', package])
