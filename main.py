b9074904ce5faddd9c665d09e5c9f70bae1fba8f
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-10-29 19:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ warnings.filterwarnings("ignore")
 import snowflake.connector
 
 
+def sf_authenticate(user, account, warehouse):
+  conn = snowflake.connector.connect(
+    user="anirudh.batra@theporter.in",
+    account="ss29587.ap-southeast-1",
+    authenticator="externalbrowser",
+    warehouse="WH_PROD_2_WHEELERS_XS"
+  )
+
 def create_alert():
   print('hello!')
