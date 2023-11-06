940f9ea3cdcdbf8142fbff616a5775b9f0fad843
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-11-06 15:17:28 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,7 @@
+cron_map = {
+  'Daily at 7AM': '0 7 * * *',
+  'Daily at 10AM': '0 10 * * *',
+  'Daily at 7PM': '0 19 * * *',
+  'Weekly on Mondays at 10AM': '0 10 * * MON',
+  'Weekly on Fridays at 10AM': '0 10 * * FRI'
+}
