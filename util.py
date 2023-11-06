911697f7f9f71402500cfb5bc14f38ade58afedc
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-11-06 13:07:41 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,10 +1,13 @@
-
-
+import pandas as pd
+import numpy as np
+import io
+from google.colab import files
+import math
+from dateutil import relativedelta
+import warnings
+import snowflake.connector
 
 def sf_authenticate(user, account, warehouse):
-  current_user = user
-  current_account = account
-  current_warehouse = warehouse
   conn = snowflake.connector.connect(
     user = user,
     account = account,
