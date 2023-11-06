377eae16cd906c1d577a419b82bf26c65e8721da
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-11-06 16:25:26 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,3 +5,5 @@ cron_map = {
   'Weekly on Mondays at 10AM': '0 10 * * MON',
   'Weekly on Fridays at 10AM': '0 10 * * FRI'
 }
+
+capability_unavailable_msg = 'This capability is coming soon! For more information, please reach out to anirudh.batra@theporter.in'
