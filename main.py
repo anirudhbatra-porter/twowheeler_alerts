c47acc57c6f505aab8ba6bbf1928436dd382b32c
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-11-08 13:53:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,33 +31,31 @@ def create_kpi_alert(alert_name, cron_frequency, cron_expression, email_list, kp
   print(constants.capability_unavailable_msg)
 
 def create_alert(alert_type, alert_name, cron_frequency, cron_expression, email_list, kpi_query=None, validation_query=None, report_table_query=None):
-try:
-  validation_flag, fail_reason = utils.validate_alert(alert_type, cron_frequency, cron_expression, email_list, validation_query, report_table_query, kpi_query, current_warehouse, conn)
-  
-  if !validation_flag:
-    Raise Exception(f"Validation failed with reason: {fail_reason}")
+  try:
+    validation_flag, fail_reason = utils.validate_alert(alert_type, cron_frequency, cron_expression, email_list, validation_query, report_table_query, kpi_query, current_warehouse, conn)
     
-  check_table_sql = utils.read_sql_file('check_table.sql')
-  check_table_flag = utils.fetch_data(check_table_sql, conn).flag.iloc[0]
-
-  if !check_table_flag:
-    create_table_sql = utils.read_sql_file('create_alerts_table.sql')
-    create_table_result = utils.fetch_data(create_table_sql, conn)
-
-  if cron_frequency <> 'Other (specify CRON expression)' and cron_expression == '':
-    cron_expression = constants.cron_map[cron_frequency]
-
-  if !utils.validate_cron_expression(cron_expression):
-    return 'Please enter a valid CRON expression'
-
-  if alert_type == 'Validation':
-    create_validation_alert(alert_name, cron_frequency, cron_expression, email_list, validation_query, report_table_query)
-  else:
-    create_kpi_alert(alert_name, cron_frequency, cron_expression, email_list, kpi_query)
+    if !validation_flag:
+      Raise Exception(f"Validation failed with reason: {fail_reason}")
+      
+    check_table_sql = utils.read_sql_file('check_table.sql')
+    check_table_flag = utils.fetch_data(check_table_sql, conn).flag.iloc[0]
   
-  When inputs are given and this is submitted, add this entry to a table, execute a query to create the procedure (?) and task, 
-  prints a success message and shows the task ID
-except(Exception as e):
-  print(f"Ran into an exception: {e}")
+    if !check_table_flag:
+      create_table_sql = utils.read_sql_file('create_alerts_table.sql')
+      create_table_result = utils.fetch_data(create_table_sql, conn)
+  
+    if cron_frequency <> 'Other (specify CRON expression)' and cron_expression == '':
+      cron_expression = constants.cron_map[cron_frequency]
+  
+    if !utils.validate_cron_expression(cron_expression):
+      return 'Please enter a valid CRON expression'
+  
+    if alert_type == 'Validation':
+      create_validation_alert(alert_name, cron_frequency, cron_expression, email_list, validation_query, report_table_query)
+    else:
+      create_kpi_alert(alert_name, cron_frequency, cron_expression, email_list, kpi_query)
+  
+  except(Exception as e):
+    print(f"Ran into an exception: {e}")
   
     
