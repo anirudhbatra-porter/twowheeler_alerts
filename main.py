ef0fe29e146f8a2eaf36d88c5af6f2030c1f6192
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-11-08 14:27:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def sf_authenticate(user, account, warehouse):
 def create_validation_alert(alert_name, cron_frequency, cron_expression, email_list, validation_query, report_table_query):
   procedure_name = str.upper(alert_name) + '_PROCEDURE'
   task_name = str.upper(alert_name) + '_TASK'
-  p = utils.fetch_data(constants.create_validation_procedure_query.replace('PROCEDURE_NAME', procedure_name))
+  p = util.fetch_data(constants.create_validation_procedure_query.replace('PROCEDURE_NAME', procedure_name))
   # create these functions
-  utils.create_sf_task(task_name, procedure_name, alert_name, cron_expression, constants.time_zone, email_list, validation_query, report_table_query, current_warehouse, conn)
-  utils.create_db_entry('Validation Alert', task_name, procedure_name, alert_name, cron_frequnecy, cron_expression, time_zone, current_user, email_list, validation_query, report_table_query, '', conn)
+  util.create_sf_task(task_name, procedure_name, alert_name, cron_expression, constants.time_zone, email_list, validation_query, report_table_query, current_warehouse, conn)
+  util.create_db_entry('Validation Alert', task_name, procedure_name, alert_name, cron_frequnecy, cron_expression, time_zone, current_user, email_list, validation_query, report_table_query, '', conn)
 
 def create_kpi_alert(alert_name, cron_frequency, cron_expression, email_list, kpi_query):
   print(constants.capability_unavailable_msg)
@@ -37,17 +37,17 @@ def create_alert(alert_type, alert_name, cron_frequency, cron_expression, email_
     if ~validation_flag:
       raise Exception(f"Validation failed with reason: {fail_reason}")
       
-    check_table_sql = utils.read_sql_file('check_table.sql')
-    check_table_flag = utils.fetch_data(check_table_sql, conn).flag.iloc[0]
+    check_table_sql = util.read_sql_file('check_table.sql')
+    check_table_flag = util.fetch_data(check_table_sql, conn).flag.iloc[0]
   
     if ~check_table_flag:
-      create_table_sql = utils.read_sql_file('create_alerts_table.sql')
-      create_table_result = utils.fetch_data(create_table_sql, conn)
+      create_table_sql = util.read_sql_file('create_alerts_table.sql')
+      create_table_result = util.fetch_data(create_table_sql, conn)
   
     if cron_frequency != 'Other (specify CRON expression)' and cron_expression == '':
       cron_expression = constants.cron_map[cron_frequency]
   
-    if ~utils.validate_cron_expression(cron_expression):
+    if ~util.validate_cron_expression(cron_expression):
       return 'Please enter a valid CRON expression'
   
     if alert_type == 'Validation':
