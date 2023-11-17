e596f5d68a33fd81c2c5754102a378754fa1bdac
anirudhbatra-porter <144807065+anirudhbatra-porter@users.noreply.github.com>
2023-11-17 13:24:12 +0530

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,6 +1,6 @@
 import subprocess
 
-packages_to_install = ['snowflake-connector-python==2.8.3', 'pandas', 'numpy', 'math', 'dateutil', 'warnings', 'croniter']
+packages_to_install = ['snowflake-connector-python==2.8.3', 'pandas', 'numpy', 'math', 'dateutil', 'warnings', 'croniter', 'datetime', 'io']
 
 def install_dependencies():
   for package in packages_to_install:
